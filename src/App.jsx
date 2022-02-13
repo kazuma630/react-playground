0185a772d97309b7d689c48d143d7cfb2a652ae0
Kazuma Maekawa <70053749+kazuma630@users.noreply.github.com>
2022-02-13 22:23:47 +0900

    props-prc

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import ColorMessage from "./components/ColorMessage";
 
 const App = () => {
   const onClickButton = () => alert();
-  const contentStyle = {
-    color: "bule",
-    fontSize: "18px"
-  };
+
   return (
     <>
-      <h1 style={contentStyle}>hallo world</h1>
-      <p>hallo japan</p>
+      <h1>hallo world</h1>
+      <ColorMessage color="blue" message="nicetomeetyou" />
+      <ColorMessage color="green" message="Thankyou" />
       <button onClick={onClickButton}></button>
     </>
   );
