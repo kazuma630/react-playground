66a6d857c3becd206382b3182dc63f83c9e3abaf
Kazuma Maekawa <70053749+kazuma630@users.noreply.github.com>
2022-02-14 20:11:09 +0900

    state-prc

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorMessage from "./components/ColorMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
 
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1>hallo world</h1>
       <ColorMessage color="blue" message="nicetomeetyou" />
       <ColorMessage color="green" message="Thankyou" />
-      <button onClick={onClickButton}></button>
+      <button onClick={onClickCountUp}></button>
+      <p>{num}</p>
     </>
   );
 };
