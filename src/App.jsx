ac0bed0887083fe6f6596f069c806af449a74652
Kazuma Maekawa <70053749+kazuma630@users.noreply.github.com>
2022-02-15 22:15:01 +0900

    再レンダリング

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import ColorMessage from "./components/ColorMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
 
-  const [num, setNum] = useState(0);
   return (
     <>
       <h1>hallo world</h1>
@@ -14,6 +19,8 @@ const App = () => {
       <ColorMessage color="green" message="Thankyou" />
       <button onClick={onClickCountUp}></button>
       <p>{num}</p>
+      <button onClick={onClickSwitchShowFlag}>ON/OFF</button>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
