8e035f9adb901aa8dba1ed3ecaef371ea8b14cfc
Kazuma Maekawa <70053749+kazuma630@users.noreply.github.com>
2022-02-16 21:48:38 +0900

    useEffect_pre

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorMessage from "./components/ColorMessage";
 
 const App = () => {
@@ -11,6 +11,16 @@ const App = () => {
   const onClickSwitchShowFlag = () => {
     setFaceShowFlag(!faceShowFlag);
   };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
