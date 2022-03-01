094168b9b413f9e1531986d753e6fd5b239f3e49
Kazuma Maekawa <70053749+kazuma630@users.noreply.github.com>
2022-03-01 21:21:56 +0900

    カウントイベント作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { ColorMessage } from "./components/ColorMessage";
+import { useState } from "react";
+import "./styles.css";
 
-const App = () => {
-  const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+export default function App() {
+  const [count, setCount] = useState(0);
 
   const onClickCountUp = () => {
-    setNum(num + 1);
+    setCount(count + 1);
   };
-  const onClickSwitchShowFlag = () => {
-    setFaceShowFlag(!faceShowFlag);
-  };
-
-  useEffect(() => {
-    if (num > 0) {
-      if (num % 3 === 0) {
-        faceShowFlag || setFaceShowFlag(true);
-      } else {
-        faceShowFlag && setFaceShowFlag(false);
-      }
-    }
-  }, [num]);
 
   return (
     <>
       <h1>hallo world</h1>
-      <ColorMessage color="blue" message="nicetomeetyou" />
-      <ColorMessage color="green" message="Thankyou" />
-      <button onClick={onClickCountUp}></button>
-      <p>{num}</p>
-      <button onClick={onClickSwitchShowFlag}>ON/OFF</button>
-      {faceShowFlag && <p>＼(^o^)／</p>}
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウント</button>
     </>
   );
-};
-
-export default App;
+}
